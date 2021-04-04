a860ea99218d558a05fafa34eb3b166b07efe6c3
Maurice Makaay <mmakaay1@xs4all.net>
2021-04-04 02:08:13 +0200

    Removed old stale RGB code.

--- night_light.h
+++ night_light.h
@@ -11,9 +11,6 @@ class NightLight
 {
 public:
     // Based on measurements using the original device firmware.
-    //float red   = 0.968f;
-    //float green = 0.960f;
-    //float blue  = 0.978f;
     float red   = 0.968f;
     float green = 0.968f;
     float blue  = 0.972f;
