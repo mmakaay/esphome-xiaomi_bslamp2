b690c290cfa05344df44b7e2b4dd23ba6da77271
Maurice Makaay <account+github@makaay.nl>
2021-04-21 11:42:02 +0200

    Multi touch events (#17)
    Documentation and binary sensor code quality improvements.

--- binary_sensor/__init__.py
+++ binary_sensor/__init__.py
@@ -1,7 +1,8 @@
+import re
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_FOR
 from .. import (
     bslamp2_ns, CODEOWNERS,
     CONF_FRONT_PANEL_HAL_ID, FrontPanelHAL
@@ -11,29 +12,46 @@ AUTO_LOAD = ["xiaomi_bslamp2"]
 
 CONF_PART = "part"
 
+# The identifier values match the bit values of the events as defined 
+# in ../front_panel_hal.h.
 PARTS = {
-    "ANY"          : 0,
-    "POWER_BUTTON" : 1,
-    "POWER"        : 1,
-    "COLOR_BUTTON" : 2,
-    "COLOR"        : 2,
-    "SLIDER"       : 3,
+    "POWER_BUTTON" : 0b001 << 1,
+    "POWER"        : 0b001 << 1,
+    "COLOR_BUTTON" : 0b010 << 1,
+    "COLOR"        : 0b010 << 1,
+    "SLIDER"       : 0b100 << 1,
 }
-
-def validate_part(value):
-    value = cv.string(value)
-    return cv.enum(PARTS, upper=True, space='_')(value)
 
 XiaomiBslamp2TouchBinarySensor = bslamp2_ns.class_(
     "XiaomiBslamp2TouchBinarySensor", binary_sensor.BinarySensor, cg.Component)
 
-CONFIG_SCHEMA = binary_sensor.BINARY_SENSOR_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(XiaomiBslamp2TouchBinarySensor),
-        cv.GenerateID(CONF_FRONT_PANEL_HAL_ID): cv.use_id(FrontPanelHAL),
-        cv.Optional(CONF_PART, default="ANY"): validate_part,
-    }
-).extend(cv.COMPONENT_SCHEMA)
+def validate_for(value):
+    value = cv.string(value)
+    return cv.enum(PARTS, upper=True, space='_')(value)
+
+def validate_binary_sensor(conf):
+    if CONF_PART in conf and CONF_FOR in conf:
+        raise cv.Invalid("Specify only one of [part] or [for]")
+    if CONF_PART in conf and not CONF_FOR in conf:
+        conf[CONF_FOR] = conf[CONF_PART]
+    if CONF_FOR not in conf:
+        raise cv.Invalid("'for' is a required option for [binary_sensor.xiaomi_bslamp2]")
+    return conf
+
+CONFIG_SCHEMA = cv.All(
+    binary_sensor.BINARY_SENSOR_SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(XiaomiBslamp2TouchBinarySensor),
+            cv.GenerateID(CONF_FRONT_PANEL_HAL_ID): cv.use_id(FrontPanelHAL),
+            # This option is not advertised in the documentation. It must be
+            # considered deprecated. I'm not announcing it as such yet. Not sure
+            # if it's useful to do so.
+            cv.Optional(CONF_PART): validate_for,
+            cv.Optional(CONF_FOR): validate_for,
+        }
+    ).extend(cv.COMPONENT_SCHEMA),
+    validate_binary_sensor,
+)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -42,4 +60,4 @@ def to_code(config):
 
     front_panel_hal_var = yield cg.get_variable(config[CONF_FRONT_PANEL_HAL_ID])
     cg.add(var.set_parent(front_panel_hal_var))
-    cg.add(var.set_part(config[CONF_PART]))
+    cg.add(var.set_for(config[CONF_FOR]))
--- binary_sensor/touch_binary_sensor.h
+++ binary_sensor/touch_binary_sensor.h
@@ -18,38 +18,33 @@ public:
         front_panel_ = front_panel;
     }
 
-    void set_part(int part) {
-        part_ = part;
+    void set_for(int part) {
+        for_ = part;
     }
 
     void setup() {
         front_panel_->add_on_event_callback(
             [this](EVENT ev) {
-                // Filter events by part, when requested.
-                if (part_ > 0) {
-                    if ((ev & FLAG_PART_MASK) != (part_ << FLAG_PART_SHIFT)) {
-                        return;
-                    }
+                auto part_in_event = ev & FLAG_PART_MASK;
+                if (for_ == 0 || part_in_event == for_) {
+                    auto new_state = (ev & FLAG_TYPE_MASK) == FLAG_TYPE_TOUCH;
+                    this->publish_state(new_state);
                 }
-                // Publish the new state, based on the touch/release status..
-                auto on_or_off = (ev & FLAG_TYPE_MASK) == FLAG_TYPE_TOUCH;
-                this->publish_state(on_or_off); 
             }
         );
     }
 
     void dump_config() {
         ESP_LOGCONFIG(TAG, "Front panel binary_sensor:");
-        ESP_LOGCONFIG(TAG, "  Part: %s (id %d)", 
-            (part_ == 1 ? "power button" :
-             part_ == 2 ? "color button" :
-             part_ == 3 ? "slider" : "any"),
-            part_);
+        ESP_LOGCONFIG(TAG, "  For: %s",
+            for_ & FLAG_PART_POWER ? "power button" :
+            for_ & FLAG_PART_COLOR ? "color button" :
+            for_ & FLAG_PART_SLIDER ? "slider" : "ERR");
     }
 
 protected:
     FrontPanelHAL *front_panel_;
-    EVENT part_;
+    EVENT for_ = 0;
 };
     
 } // namespace bslamp2
