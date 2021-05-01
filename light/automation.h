aa3f8add4b4e04d6a538b086aa65ad4b4673e65e
Maurice Makaay <mmakaay1@xs4all.net>
2021-05-02 01:36:22 +0200

    Implement disco-mode actions.

--- light/automation.h
+++ light/automation.h
@@ -2,7 +2,9 @@
 
 #include "esphome/core/automation.h"
 #include "esphome/core/component.h"
+#include "interfaces.h"
 #include "light_output.h"
+#include "light_state.h"
 #include "presets.h"
 #include <cmath>
 
@@ -27,6 +29,50 @@ class BrightnessTrigger : public Trigger<float> {
 
  protected:
   float last_brightness_ = -1.0f;
+};
+
+template<typename... Ts> class DiscoAction : public Action<Ts...> {
+ public:
+  explicit DiscoAction(LightStateDiscoSupport *parent) : parent_(parent) {}
+
+  TEMPLATABLE_VALUE(bool, disco_state)
+  TEMPLATABLE_VALUE(bool, state)
+  TEMPLATABLE_VALUE(uint32_t, transition_length)
+  TEMPLATABLE_VALUE(uint32_t, flash_length)
+  TEMPLATABLE_VALUE(float, brightness)
+  TEMPLATABLE_VALUE(float, red)
+  TEMPLATABLE_VALUE(float, green)
+  TEMPLATABLE_VALUE(float, blue)
+  TEMPLATABLE_VALUE(float, color_temperature)
+  TEMPLATABLE_VALUE(std::string, effect)
+
+  void play(Ts... x) override {
+    if (this->disco_state_.has_value()) {
+      auto p = this->disco_state_.optional_value(x...);
+      if (!*p) {
+        parent_->disco_stop();
+        return;
+      }
+    }
+
+    auto call = this->parent_->make_disco_call(false);
+    call.set_state(this->state_.optional_value(x...));
+    call.set_brightness(this->brightness_.optional_value(x...));
+    call.set_red(this->red_.optional_value(x...));
+    call.set_green(this->green_.optional_value(x...));
+    call.set_blue(this->blue_.optional_value(x...));
+    call.set_color_temperature(this->color_temperature_.optional_value(x...));
+    call.set_effect(this->effect_.optional_value(x...));
+    call.set_flash_length(this->flash_length_.optional_value(x...));
+    call.set_transition_length(this->transition_length_.optional_value(x...));
+
+    // Force the light to update right now, not in the next loop.
+    call.perform();
+    parent_->disco_apply();
+  }
+
+ protected:
+  LightStateDiscoSupport *parent_;
 };
 
 template<typename... Ts> class ActivatePresetAction : public Action<Ts...> {
